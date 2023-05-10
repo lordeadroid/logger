4e41491a7bcd6a96422a1adab43d0f792440107f
Rishabh <lordeadroid@users.noreply.github.com>
2023-05-11 00:55:59 +0530

    added logger function

--- main.js
+++ main.js
@@ -1,1 +1,21 @@
+const fs = require("fs");
 
+const append = function (logFile, logs, todayLog) {
+  let newLog = "";
+
+  newLog += logs;
+  newLog += "\n";
+  newLog += todayLog;
+
+  fs.writeFileSync(logFile, newLog);
+}
+
+const main = function () {
+  const logFile = "log.txt";
+  const logs = fs.readFileSync(logFile, "utf-8");
+
+  const todayLog = process.argv[2];
+  append(logFile, logs, todayLog);
+}
+
+main();
