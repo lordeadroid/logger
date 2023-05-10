2afcf8d4ec74907e8457727937153990f3d6fe00
Rishabh <lordeadroid@users.noreply.github.com>
2023-05-11 01:30:12 +0530

    added date feature in log

--- main.js
+++ main.js
@@ -1,7 +1,8 @@
 const fs = require("fs");
 
-const append = function (logFile, logs, todayLog) {
+const append = function (logFile, logs, today, todayLog) {
   let newLog = logs;
+  newLog += today + " : ";
   newLog += todayLog;
   newLog += "\n";
 
@@ -12,8 +13,11 @@ const main = function () {
   const logFile = "log.txt";
   const logs = fs.readFileSync(logFile, "utf-8");
 
+  const date = new Date();
+  const today = `${date.toDateString()}`;
+
   const todayLog = process.argv[2];
-  append(logFile, logs, todayLog);
+  append(logFile, logs, today, todayLog);
 }
 
 main();
