a4640439e25d5c83150af1bc51597b8bd202e40b
Rishabh <lordeadroid@users.noreply.github.com>
2023-05-13 19:45:26 +0530

    implemented function that can tell when it is new date

--- main.js
+++ main.js
@@ -1,8 +1,20 @@
 const fs = require("fs");
+const { config } = require("process");
 
 const getTimeStamp = () => {
   const date = new Date();
   return `${date.toDateString()}`;
+};
+
+const isNewDay = () => {
+  const configFile = fs.readFileSync("./.config", "utf-8");
+  const config = JSON.parse(configFile);
+  const referenceTime = config.date;
+  const day = 864000000;
+  const today = new Date();
+  const currentTime = today.getTime();
+
+  return currentTime - referenceTime > day;
 };
 
 const logData = function (logFile, entryToLog) {
@@ -18,5 +30,4 @@ const main = function () {
 
   logData(logFile, entryToLog);
 }
-
-main();
+console.log(isNewDay());
