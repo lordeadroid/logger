60efa62aaf0a24187f905ccbfac2b8621732aad3
Rishabh <lordeadroid@users.noreply.github.com>
2023-05-15 00:19:06 +0530

    modified functions so that is create different separate logs according to date

--- main.js
+++ main.js
@@ -1,26 +1,31 @@
 const fs = require("fs");
 
-const getTimeStamp = () => {
-  const date = new Date();
-  return `${date.toDateString()}`;
-};
-
-const isNewDay = () => {
-  const configFile = fs.readFileSync("./.config", "utf-8");
-  const {date} = JSON.parse(configFile);
-  const today = new Date();
-
-  return date === today.getDate();
-};
-
 const logData = (logFile, entryToLog) => fs.appendFileSync(logFile, entryToLog);
+const isSameDay = (date, currentDate) => date === currentDate;
 
-const main = function () {
+const getTimeStamp = () => {
+  const configFile = "./.config";
+  const data = fs.readFileSync(configFile, "utf-8");
+  const config = JSON.parse(data);
+  const date = config.date;
+  const today = new Date();
+  const currentDate = today.toDateString();
+
+  if (isSameDay(date, currentDate)) {
+    return `\t\t->`;
+  }
+
+  config.date = currentDate;
+  fs.writeFileSync(configFile, JSON.stringify(config));
+  
+  return `\n${currentDate} :\n\t\t->`;
+};
+
+const main = () => {
   const logFile = "log.txt";
-  const logs = fs.readFileSync(logFile, "utf-8");
-  const dataLog = process.argv[2];
-  const timeStamp = getTimeStamp();
-  const entryToLog = `${timeStamp} : ${dataLog}\n`;
+  const dataToLog = process.argv[2];
+
+  const entryToLog = `${getTimeStamp()} ${dataToLog}\n`;
 
   logData(logFile, entryToLog);
 };
